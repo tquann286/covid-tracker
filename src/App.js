c541cb7b510f89fb5a086545d324990c124b0c8e
Quan Trung <quantrung286@gmail.com>
2022-02-19 14:08:40 +0700

    up

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
 import React from 'react'
 
+import { Cards, Chart, CountryPicker } from './components'
+import styles from './App.module.css'
+import { fetchData } from './api'
+
 class App extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>App</h1>
-      </div>
-    )
-  }
+	async componentDidMount() {
+		const data = await fetchData()
+
+		console.log(data)
+	}
+
+	render() {
+		return (
+			<div className={styles.container}>
+				<Cards />
+				<Chart />
+				<CountryPicker />
+			</div>
+		)
+	}
 }
 
 export default App
